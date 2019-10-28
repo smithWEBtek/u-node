b9224cc4284ea81bb1d05a314727349bbbfab45d
smithwebtek <brad@smithwebtek.com>
2019-10-28 17:54:56 -0400

    Restart course, 1 folder for each video

---
+++ 10-importing-your-own-files/app.js
@@ -0,0 +1,3 @@
+const getNotes = require('./notes.js')
+
+getNotes()
---
+++ 10-importing-your-own-files/notes.js
@@ -0,0 +1,7 @@
+console.log('notes.js ran ... ')
+
+const getNotes = function () {
+  return "Your notes ..."
+}
+
+module.exports = getNotes
